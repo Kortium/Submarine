c0e8c0f56b7d1b39267568254a3bebb7b35ca5c8
Kortium <ieslondor@gmail.com>
2014-02-13 02:55:38 +0400

    13022014
    Orientation starter

--- observations_submerged.m
+++ observations_submerged.m
@@ -32,8 +32,7 @@ P = angle(2);
 
 % incremental tests for bounding semi-circle
 ii= find(abs(dx) < rmax & abs(dy) < rmax & abs(dz) < rmax ... % bounding cube
-        & abs(T- atan2(dy,dx)) < pi/3 ...     % bounding azimuth    
-        & abs(P - atan2(dz,sqrt(dx.^2+dy.^2))-pi/6) < pi/3 ...       %bounding elevation        
+        & abs(T- atan2(dy,dx)) < pi/3 ...     % bounding azimuth          
         & (dx.^2 + dy.^2 + dz.^2) < rmax^2);           % bounding sphere
   
 
@@ -50,13 +49,15 @@ idf= idf(ii);
 
 function z= compute_range_bearing(x,lm)
 % Compute exact observation
-dx= lm(1,:) - x(1);
-dy= lm(2,:) - x(2);
-dz= lm(3,:) - x(3);
+dx= x(1) - lm(1,:);
+dy= x(2) - lm(2,:);
+dz= x(3) - lm(3,:);
 Rho = sqrt(dx.^2 + dy.^2 + dz.^2); 
-[angle(1), angle(2), angle(3)] = quat2angle(x(4:7));
-T = angle(1);
-P = angle(2);
-z= [pi_to_pi(atan2(dy,dx)-T);
-    pi_to_pi(atan2(dz,sqrt(dx.^2+dy.^2)) - P);
-    Rho];
+DCM = quat2dcm([x(4) -x(5) -x(6) -x(7)]);
+z=zeros(3,size(lm,2));
+for i =1:size(lm,2)
+cfc =  DCM*[dx(i);dy(i);dz(i)];
+z(:,i)= [pi_to_pi(atan2(cfc(2),cfc(1)));
+pi_to_pi(atan2(cfc(3),sqrt(cfc(1)^2+cfc(2)^2)));
+Rho(i)];
+end
