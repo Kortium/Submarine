10ba6baab24cccb3733362064889150a41953524
Kortium <ieslondor@gmail.com>
2014-04-21 00:38:59 +0400

    21042014
    One more result

--- add_control_noise.m
+++ add_control_noise.m
@@ -5,6 +5,6 @@ function [V,W]= add_control_noise(V,W,Q, addnoise)
 if addnoise == 1
     V= V + randn(1)*sqrt(Q(1,1));
     W(1)= W(1) + randn(1)*sqrt(Q(2,2));
-    W(2)= W(2) + randn(1)*sqrt(Q(3,3));
+    W(2)= W(2);
 end
 
